eed973679ac779ff61e4e857d56a1fa607cd1709
Andrew Yatsenko <divinkas@gmail.com>
2020-09-15 08:34:04 +0300

    Replaced NavigationView on BottomNavigationView

--- app/src/main/java/com/divinkas/app/words/ui/main/MainActivity.kt
+++ app/src/main/java/com/divinkas/app/words/ui/main/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.divinkas.app.words.ui.main
 
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.divinkas.app.words.R
 import com.divinkas.app.words.databinding.ActivityMainBinding
 import com.divinkas.app.words.helper.ext.binding
@@ -20,31 +16,5 @@ class MainActivity : AbstractScreenActivity() {
     }
 
     private fun initView() {
-        setSupportActionBar(binding.toolbar)
-        val toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        val navController = findNavController(R.id.nav_graph_fragment)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            title = destination.label
-        }
-
-        binding.navView.setupWithNavController(navController)
-    }
-
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START))
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        else run {
-            super.onBackPressed()
-        }
     }
 }
